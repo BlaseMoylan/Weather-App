2d6193bfd0534695e8cdf9156d0c3f94a43ae4d2
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-07-14 14:42:22 -0400

    📦 NEW: node installed Sassy for CSS

--- weather-app-frontend/src/App.js
+++ weather-app-frontend/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import React, { useEffect, useState } from 'react';
 import Weather from './components/weather';
 import { Dimmer, Loader } from 'semantic-ui-react';
