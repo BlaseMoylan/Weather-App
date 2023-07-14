4cde71da4c6b432aeaf980a50a545fb48ded8c2c
Backinblacker <coachblacker@gmail.com>
2023-07-14 13:26:22 -0600

    🐛 FIX: API Fixed

--- weather-app-frontend/src/App.js
+++ weather-app-frontend/src/App.js
@@ -27,8 +27,7 @@ export default function App() {
 
       try {
         const response = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather/?lat=${lat}&lon=${long}&units=imperial&APPID=5d9c6432ec75c55bcee54659717e0ae1`
-          // 'https://api.openweathermap.org/data/2.5/weather/?lat=80&lon=80&units=metric&APPID=5d9c6432ec75c55bcee54659717e0ae1'
+          `${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=imperial&APPID=${process.env.REACT_APP_API_KEY}`
         );
         console.log("dlahghalglaldflalsldkhglahl")
         const result = response.data;
