e6c67ee6414c7dc95499d1ff92e0821d20ccaf1c
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-07-18 16:30:44 -0400

    📦 NEW: added mapbox-search-bar to dependences and added to Navbar.jsx

--- weather-app-frontend/src/components/Navbar/Navbar.jsx
+++ weather-app-frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,14 @@
+import { SearchBox } from '@mapbox/search-js-react';
 import './Navbar.scss'
 
 const Navbar = () => {
     return (
         <nav>
-
+            <div className='search-area'>
+                {/* <SearchBox value='' theme={{variables:{boxShadow:'none', padding}, cssText:'.SearchBox{width:250px}'}}  options={{types:'city', language:'en'}} accessToken='pk.eyJ1IjoiYy1yb21hbmNhbnRyZWxsIiwiYSI6ImNsZ3h6MmliOTA0Z3IzZXBpZWpqOWNtbzQifQ.AA1tCkKxeoIpw4YGUsYTJQ' /> */}
+            </div>
+            <h1>MySkies</h1>
+            <a href='#'>login/sign-up</a>
         </nav>
     );
 }
