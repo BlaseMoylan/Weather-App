ded90d5c743515f24caa38382fca8cc68fdfd153
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-07-19 12:54:17 -0400

    👌 IMPROVE: uncommented component

--- weather-app-frontend/src/components/Navbar/Navbar.jsx
+++ weather-app-frontend/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav>
             <div className='search-area'>
-                {/* <SearchBox value='' theme={{variables:{boxShadow:'none', padding}, cssText:'.SearchBox{width:250px}'}}  options={{types:'city', language:'en'}} accessToken='pk.eyJ1IjoiYy1yb21hbmNhbnRyZWxsIiwiYSI6ImNsZ3h6MmliOTA0Z3IzZXBpZWpqOWNtbzQifQ.AA1tCkKxeoIpw4YGUsYTJQ' /> */}
+                <SearchBox value='' theme={{variables:{boxShadow:'none', padding}, cssText:'.SearchBox{width:250px}'}}  options={{types:'city', language:'en'}} accessToken='pk.eyJ1IjoiYy1yb21hbmNhbnRyZWxsIiwiYSI6ImNsZ3h6MmliOTA0Z3IzZXBpZWpqOWNtbzQifQ.AA1tCkKxeoIpw4YGUsYTJQ' />
             </div>
             <h1>MySkies</h1>
             <a href='#'>login/sign-up</a>
