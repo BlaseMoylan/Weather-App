ad18229c4fa7844f2d55e536ddefa3512eeb6373
Blase Moylan <moylanblase@gmail.com>
2023-07-18 21:03:13 -0700

    📦 NEW: notes plan and basic design

---
+++ weather-app-frontend/src/components/body/body.js
@@ -0,0 +1,26 @@
+// this will be on a set default to the current accounts home location (this funtionality is not configured yet)
+// this will also be used by the search funtionality ( i am currently working on this )
+
+
+// Takes in location
+    // figure out how to get 4 day forcast ( i am currently working on this )
+
+// make a call for the 4 day forcast
+
+// < this is all in the return
+// sends in each days info to the card component
+    // maping over each day and returning the custom card for that
+// displays each day >
+
+import { useState } from "react";
+import React from "react";
+import axios from "axios";
+import "./body.css"
+
+export default function body({location}){
+    // the default useState will need to be set to the home location of the account ( I will work on this later )
+    // 
+    return (
+        <div>body</div>
+    )
+}
