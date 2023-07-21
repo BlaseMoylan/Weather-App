88a0ab7c21cae113391d84d6c67448cc5d4bfc25
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-07-21 13:12:17 -0400

    📦 NEW: Modals now have a exit button

--- weather-app-frontend/src/components/Modal/Modal.jsx
+++ weather-app-frontend/src/components/Modal/Modal.jsx
@@ -1,10 +1,23 @@
+import { useRef } from 'react';
 import './Modal.scss'
 
 const Modal = ({close, show, children}) => {
+
+    const modalRef = useRef()
+
+    function handleOutsideClick(event){
+        if(event.target === modalRef.current){
+            close()
+        }
+    }
+
     return (
         <>{show ?
-            <div className='modal-exit'>
-                {children}
+            <div ref={modalRef} className='modal-exit' onClick={(event)=>handleOutsideClick(event)}>
+                <div className='modal-space'>
+                    <div className='modal-close' onClick={close}>x</div>
+                    {children}
+                </div>
             </div>
         :null}</>
     );
