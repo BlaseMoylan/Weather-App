1256bf45c803ab6eb74ab53dd2c7c22a2543f8df
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-08-09 12:30:54 -0400

    📦 NEW: Routes lead to login and register

--- weather-app-frontend/src/App.js
+++ weather-app-frontend/src/App.js
@@ -1,5 +1,10 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+
+
 import HomePage from './pages/HomePage';
+
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 import './App.scss';
 
 /**
@@ -14,8 +19,8 @@ export default function App() {
       <Routes>
         <Route path='/home' element={<HomePage />} />
         <Route path='/auth'>
-          <Route path='/login' />
-          <Route path='/register' />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
           <Route path='/forgotpassword' />
           <Route path='/resetpassword /:resetCode' />
         </Route>
