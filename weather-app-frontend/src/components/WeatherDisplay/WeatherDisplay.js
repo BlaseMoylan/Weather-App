51c7197da7e02c019a4d6d50f9527d1535035689
Blase Moylan <moylanblase@gmail.com>
2023-08-21 11:04:21 -0700

    📦 NEW: rando

--- weather-app-frontend/src/components/WeatherDisplay/WeatherDisplay.js
+++ weather-app-frontend/src/components/WeatherDisplay/WeatherDisplay.js
@@ -164,7 +164,7 @@ export default function WeatherDisplay({ lat, long }) {
             <div className="forecasted">
                 {/* Display forecasted weather cards */}
                 {/* look into mapping over the card component */}
-                {/* the card component still needs to be made and imported for this to work!!! */}
+                {/* the card component still needs to be made and imported for this to work!!!! */}
                 <WeatherCard minMax={getTemperatureStats(day1)} averageHumidity={getAverageHumidity(day1)} weatherDescription={getMostCommonWeatherDescription(day1)} date={day1[0].dt_txt.split(" ")[0]} />
                 <WeatherCard minMax={getTemperatureStats(day2)} averageHumidity={getAverageHumidity(day2)} weatherDescription={getMostCommonWeatherDescription(day2)} date={day2[0].dt_txt.split(" ")[0]} />
                 <WeatherCard minMax={getTemperatureStats(day3)} averageHumidity={getAverageHumidity(day3)} weatherDescription={getMostCommonWeatherDescription(day3)} date={day3[0].dt_txt.split(" ")[0]} />
