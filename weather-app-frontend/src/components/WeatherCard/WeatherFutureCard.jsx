b4a73671d65e260b18e939190c3511e7b5e302a6
Backinblacker <coachblacker@gmail.com>
2023-08-22 13:23:46 -0600

    📦 NEW: Future card started CSS Next step

--- weather-app-frontend/src/components/WeatherCard/WeatherFutureCard.jsx
+++ weather-app-frontend/src/components/WeatherCard/WeatherFutureCard.jsx
@@ -1,5 +1,15 @@
-const WeatherFutureCard = () => {
-    return (null);
-}
+const WeatherFutureCard = ({ minMax, averageHumidity, weatherDescription, date }) => {
+    const refresh = () => {
+        window.location.reload();
+      };
+    return (
+        <div className="???">
+        <p>Date: {date}</p>
+        <p>Temperature: {Math.round(minMax.maxTemp)}°F / {Math.round(minMax.minTemp)}°F</p>
+        <p>Humidity: {Math.round(averageHumidity)}%</p>
+        <p>Weather: {weatherDescription}</p>
+        </div>
+    );
+    };     
 
 export default WeatherFutureCard;
